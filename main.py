3ffd02012586edc6cad9ada274eb35917ff0430d
Mihai Vladu <vladuomihai@gmail.com>
2023-02-16 10:17:47 +0200

    Python basics.

---
+++ main.py
@@ -0,0 +1,32 @@
+# # asdhasidhaisudhai
+# print("Message #1")
+# print("Message #2")  # this is an inline comment
+#
+# # for ....:
+# #     print('this is inside for')
+# #     print('this is inside for')
+
+# print(1, type(1))
+# print(2.5, type(2.5))
+# print(2.5+5j, type(2.5+5j))
+
+# print(1, type(1), float(1), complex(1))
+# print(2.5, type(2.5), int(2.5), complex(2.5))
+# print(3+5j, type(3+5j), float(3+5j))
+
+# print(7 / 4, 7 // 4, 7 % 4)
+
+# print(5 == 5, 4 == 8)
+
+# print(5 == 5    and    4 == 8)
+# #     True              False
+# print(4 == 8    and    5 == 5)
+# #     False              True
+
+# print(5 == 5    or    4 == 8)
+# #     True              False
+# print(4 == 8    or    5 == 5)
+# #     False              True
+
+# print(not True, not False)
+print(not (5 == 5 or 4 == 8))
